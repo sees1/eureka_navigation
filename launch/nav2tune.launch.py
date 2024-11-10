a1fbe4481fed50c1ce8c61128c671f6f321ca77a
Sees <Vsevolod.Egorov@ronavi.ru>
2024-11-10 18:28:28 +0300

    feat: add new nav config + unused rgbd odometry

--- launch/nav2tune.launch.py
+++ launch/nav2tune.launch.py
@@ -58,13 +58,101 @@ def launch_setup(context, *args, **kwargs):
         ),
     ])
 
+    rgbd_parameters = [{
+        "frame_id": "base_footprint",
+        "subscribe_depth": True,
+        "subscribe_rgb": True,
+        "approx_sync": True,
+        "approx_sync_max_interval": 0.01,
+        "publish_tf": False,
+        "wait_imu_to_init": False,
+        "publish_null_when_lost": False,
+        "qos": 2,
+        "qos_camera_info": 2,
+
+        # 0=TORO, 1=g2o, 2=GTSAM and 3=Ceres
+        "Optimizer/Strategy": "2",
+        "Optimizer/GravitySigma": "0.3",
+
+        # 0=Frame-to-Map (F2M) 1=Frame-to-Frame (F2F) 2=Fovis 3=viso2 4=DVO-SLAM 5=ORB_SLAM2 6=OKVIS 7=LOAM 8=MSCKF_VIO 9=VINS-Fusion 10=OpenVINS 11=FLOAM 12=Open3D
+        "Odom/Strategy": "0",
+        "Odom/ResetCountdown": "1",
+        "Odom/Holonomic": "false",
+        # 0=No filtering 1=Kalman filtering 2=Particle filtering
+        "Odom/FilteringStrategy": "0",
+        "Odom/ParticleSize": "400",
+        "Odom/GuessMotion": "true",
+        "Odom/AlignWithGround": "false",
+
+        "GFTT/MinDistance": "5.0",
+        "GFTT/QualityLevel": "0.001",
+        "GFTT/BlockSize": "4",
+        "GFTT/UseHarrisDetector": "false",
+        "GFTT/K": "0.04",
+
+        "SURF/Extended": "true",
+        "SURF/HessianThreshold": "500",
+        "SURF/Octaves": "4",
+        "SURF/OctaveLayers": "4",
+        "SURF/Upright": "false",
+        "SURF/GpuVersion": "false",
+        "SURF/GpuKeypointsRatio": "0.01",
+
+        "SIFT/NFeatures": "1000",
+        "SIFT/NOctaveLayers": "4",
+        "SIFT/RootSIFT": "false",
+
+        "FREAK/OrientationNormalized": "true",
+        "FREAK/ScaleNormalized": "true",
+        "FREAK/PatternScale": "30",
+        "FREAK/NOctaves": "4",
+
+        "KAZE/Extended": "true",
+        "KAZE/Upright": "false",
+        "KAZE/NOctaves": "4",
+        "KAZE/NOctaveLayers": "4",
+        # 0=DIFF_PM_G1, 1=DIFF_PM_G2, 2=DIFF_WEICKERT or 3=DIFF_CHARBONNIER
+        "KAZE/Diffusivity": "1",
+
+        "BRIEF/Bytes": "64",
+
+        "Vis/EstimationType": "1",
+        "Vis/ForwardEstOnly": "true",
+        # 0=SURF 1=SIFT 2=ORB 3=FAST/FREAK 4=FAST/BRIEF 5=GFTT/FREAK 6=GFTT/BRIEF 7=BRISK 8=GFTT/ORB 9=KAZE 10=ORB-OCTREE 11=SuperPoint 12=SURF/FREAK 13=GFTT/DAISY 14=SURF/DAISY 15=PyDetector
+        "Vis/FeatureType": "8",
+        "Vis/DepthAsMask": "true",
+        "Vis/CorGuessWinSize": "40",
+        "Vis/MaxFeatures": "1000",
+        "Vis/MinDepth": "0.0",
+        "Vis/MaxDepth": "0.0",
+        # 0=Features Matching, 1=Optical Flow
+        "Vis/CorType": "0",
+        # kNNFlannNaive=0, kNNFlannKdTree=1, kNNFlannLSH=2, kNNBruteForce=3, kNNBruteForceGPU=4, BruteForceCrossCheck=5, SuperGlue=6, GMS=7
+        "Vis/CorNNType": "1"
+    }]
+
+    rgbd_odometry = Node(
+            package="rtabmap_odom",
+            executable="rgbd_odometry",
+            output="log",
+            parameters=rgbd_parameters,
+            remappings=[
+                ("rgb/image", "/camera/image_raw"),
+                ("rgb/camera_info", "/camera/camera_info"),
+                ("depth/image", "/camera/depth/image_raw"),
+                ("imu", "/imu"),
+                ("odom", "odom_rgbd")
+            ],
+            arguments=["--ros-args", "--log-level", "Error"]
+    )
+
     rtabmap_slam = Node(
         package='rtabmap_slam',
         executable='rtabmap',
         name="rtabmap",
         output="screen",
         parameters=[{
-            "subscribe_depth": False,
+            "subscribe_depth": True,
             "subscribe_rgbd": False,
             "subscribe_rgb": False,
             "subscribe_stereo": False,
@@ -72,7 +160,7 @@ def launch_setup(context, *args, **kwargs):
             "subscribe_scan_cloud": True,
             "subscribe_user_data": False,
             "subscribe_odom_info": False,
-            "frame_id": 'base_link',
+            "frame_id": 'base_footprint',
             "map_frame_id": 'map',
             "odom_frame_id": 'odom',
             "publish_tf": True,
@@ -89,10 +177,10 @@ def launch_setup(context, *args, **kwargs):
             "config_path": '',
             "topic_queue_size": 10,
             "sync_queue_size": 10,
-            "qos_image": 1,
+            "qos_image": 2,
             "qos_scan": 2,
             "qos_odom": 1,
-            "qos_camera_info": 1,
+            "qos_camera_info": 2,
             "qos_imu": 2,
             "qos_gps": 1,
             "qos_user_data": 1,
@@ -112,14 +200,18 @@ def launch_setup(context, *args, **kwargs):
         }],
         remappings=[
             ("/grid_prob_map", "/map"),
-            ("scan_cloud", "/camera/points")
+            ("scan_cloud", "/camera/points"),
+            ("rgb/image", "/camera/image_raw"),
+            ("rgb/camera_info", "/camera/camera_info"),
+            ("depth/image", "/camera/depth/image_raw")
         ],
         arguments=["--delete_db_on_start"],
     )
 
     return [nav2,
             SetParameter(name='use_sim_time', value=True),
-            rtabmap_slam
+            rtabmap_slam,
+            # rgbd_odometry
     ]
 
 
